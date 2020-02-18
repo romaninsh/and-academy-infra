9b7049241be494a0957550d72509095c46652497
pindey <18559548+pindey@users.noreply.github.com>
2020-02-18 10:06:21 +0000

    Added Alex NS records (#4)
    * Added Alex NS records
    
    * Update dns.tf
    
    Co-authored-by: Romans Malinovskis <me@nearly.guru>

--- dns.tf
+++ dns.tf
@@ -42,3 +42,17 @@ resource "aws_route53_record" "splink-zone" {
   ]
 }
 */
+
+  
+resource "aws_route53_record" "alex-zone" {
+  name = "alex.${var.dns}"
+  zone_id = aws_route53_zone.main.id
+  type = "NS"
+  ttl = 900
+  records = [
+    "ns-1379.awsdns-44.org",
+    "ns-1583.awsdns-05.co.uk",
+    "ns-43.awsdns-05.com",
+    "ns-716.awsdns-25.net"
+  ]
+}
